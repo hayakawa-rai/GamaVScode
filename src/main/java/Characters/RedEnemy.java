704d460ad3df1b0bc84592802bd4e702a74fe27e
EMD-568 <EMD-568@EMD568>
2026-06-19 13:07:07 +0900

    RedEnemy途中経過　水野

--- src/main/java/Characters/RedEnemy.java
+++ src/main/java/Characters/RedEnemy.java
@@ -1,21 +1,38 @@
 package Characters;
 
+//最短距離で追跡する敵 赤
 public class RedEnemy extends Enemy {
 
-	public RedEnemy(int x, int y) {
-		super(x, y, 1);
-	}
-	
-	@Override
-	public void move(Map map, Sengoku sengoku){
-		
-		int dx = sengoku.getX() - x;
-		int dy = sengoku.getY() - y;
-		
-		if(Math.abs(dx) > Math.abs(dy)) {
-			x += Integer.signum(dx);
-		} else {
-			y += Integer.signum(dy);\
-		}
-	}
+    // 仮の初期位置（後で修正）
+    private static final int START_X = 700;
+    private static final int START_Y = 50;
+
+    //マップ右上に出現
+    public RedEnemy() {
+        super(START_X, START_Y, 1);
+    }
+
+    //追跡
+    public void move(int[][] map, Sengoku sengoku) {
+
+        // 仙石さんとの距離
+        double dx = sengoku.getX() - this.x;
+        double dy = sengoku.getY() - this.y;
+
+        // 横方向が遠い場合
+        if (Math.abs(dx) > Math.abs(dy)) {
+            this.x += Math.signum(dx) * speed;
+        }
+        // 縦方向が遠い場合
+        else {
+            this.y += Math.signum(dy) * speed;
+        }
+    }
+
+    //Characterクラスの抽象メソッド
+    //現状は使用しないため空実装
+    @Override
+    public void move(int[][] map) {
+
+    }
 }
